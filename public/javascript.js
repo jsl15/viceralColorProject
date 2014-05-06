b0e91b1c050ae4237a5d4023812f2402b45f6d60
Avery Crits-Christoph <avery_crits-christoph@brown.edu>
2014-05-06 18:00:11 -0400

    Fixed hovering

--- public/javascript.js
+++ public/javascript.js
@@ -109,30 +109,6 @@ function createSets(type){
 				              <div id='color_id4' class='color_id'></div> \
 				              <div id='color_id4_rgb' class='color_id_rgb'></div>";
 
-		$(".color").hover(function() {
-			console.log("yes");
-			var color = $(this).css("background-color");
-			var colorhex = rgb2hex(color);
-
-			var hex = $(this).next();
-			var rgb = hex.next();
-
-			hex.show();
-			rgb.show();
-
-			hex.html(colorhex);
-			rgb.html(color);
-			hex.css("color",color);
-			rgb.css("color",color);
-
-			}, function(){
-
-			var hex = $(this).next();
-			hex.hide();
-			hex.next().hide();
-
-		});
-
 		if (type == "web") { $("#web #allPalettes").append(setPalette); }
 		else { $("#mobile #allPalettes").append(setPalette); }
 	}
@@ -324,8 +300,7 @@ function changeColor(id){
 $(document).ready(function() {
 
 	socket.on('returnPalettes', function(palettes, wSet, setPhotos) {
-		// console.log('website page got palettes from server!');
-		// console.log(palettes);
+
 		wSetNum = wSet;
 
 		for (var pal=0; pal<palettes.length; pal++){
@@ -370,6 +345,29 @@ $(document).ready(function() {
 					$(this).css("backgroundColor",drag_color);
 					changeColor($(this).attr("id"));
 				} 
+			});
+
+			$(".color").hover(function() {
+				var color = $(this).css("background-color");
+				var colorhex = rgb2hex(color);
+
+				var hex = $(this).next();
+				var rgb = hex.next();
+
+				hex.show();
+				rgb.show();
+
+				hex.html(colorhex);
+				rgb.html(color);
+				hex.css("color",color);
+				rgb.css("color",color);
+
+				}, function(){
+
+				var hex = $(this).next();
+				hex.hide();
+				hex.next().hide();
+
 			});
 		},800);
 	});
